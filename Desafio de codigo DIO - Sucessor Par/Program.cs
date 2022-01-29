652a7117d5ccfc0ddeab11b1d87e4f1009a58d4c
wever <wever@DESKTOP-7KB01JA>
2022-01-29 18:22:58 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio de codigo DIO - Sucessor Par/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_de_codigo_DIO___Sucessor_Par
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int SucessorPar;
+            if (x % 2 == 0)
+            {
+                SucessorPar = x + 2;
+            }
+            else
+            {
+                SucessorPar = x + 1;
+            }
+            Console.WriteLine(SucessorPar);
+            Console.ReadKey();
+        }
+    }
+}
